ccfde92eb1b6bca8973a0134fcc99e0063ac88f0
paulbobin <bobinpaul@hotmail.com>
2026-03-21 02:48:13 +0530

    Assignment 5 .mk Commit ID Fix

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,33 +1,27 @@
-
-
 ##############################################################
 #
 # AESD-ASSIGNMENTS
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '2228d42f0a61e6ad252f34f609ce693394fdc576'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-paulbobin.git'
+AESD_ASSIGNMENTS_VERSION = b1cc66339fc499869b67065db5ce466e75f44fef
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-paulbobin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) -C $(@D)/server all
+	$(MAKE) -C $(@D)/server clean
+	$(MAKE) -C $(@D)/server \
+		CC="$(TARGET_CC)" \
+		all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket \
 		$(TARGET_DIR)/usr/bin/
 
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop \
 		$(TARGET_DIR)/etc/init.d/S99aesdsocket
-
 endef
 
 $(eval $(generic-package))
-
